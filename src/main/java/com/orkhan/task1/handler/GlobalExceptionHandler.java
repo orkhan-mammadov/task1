0a34a4e451138b2f0d35a6bcfb150dd4c7dfc385
orkhan.mammadov <orkhan.mammadov@accenture.com>
2023-02-27 17:30:53 +0200

    log to exception.

--- src/main/java/com/orkhan/task1/handler/GlobalExceptionHandler.java
+++ src/main/java/com/orkhan/task1/handler/GlobalExceptionHandler.java
@@ -3,17 +3,21 @@ package com.orkhan.task1.handler;
 import com.orkhan.task1.exception.SportEventException;
 import com.orkhan.task1.exception.SportEventNotFoundException;
 import com.orkhan.task1.model.ResponseModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = {SportEventException.class, SportEventNotFoundException.class})
     protected ResponseEntity<ResponseModel> handleConflict(SportEventException ex) {
+        log.trace(ex.getLocalizedMessage());
         return ResponseEntity.status(ex.getStatus())
                 .body(ResponseModel.builder()
                         .response(ex.getLocalizedMessage())
                         .build());
     }
+
 }
